aca57cba9af430834362cd415a289f5d94aef3e6
Marat <muraragi99@gmail.com>
2018-09-10 13:30:54 +0300

    fixed typo

--- docs/doks-theme/assets/js/scripts.js
+++ docs/doks-theme/assets/js/scripts.js
@@ -126,7 +126,7 @@
 		headings = reduceLevels( headings );
 
 		generateList( headings, true ).appendTo( '.js-sections' );
-		var ghButtonHtml = "<a href='https://github.com/icons8/lunacy-docs id='sections-button' class='btn btn--dark btn--rounded btn--w-icon btn--github js-affix'><i class='icon icon--github' style='float: left; margin-left: 0px; margin-right: 16px'></i>Suggest Edits</a>";
+		var ghButtonHtml = "<a href='https://github.com/icons8/lunacy-docs' id='sections-button' class='btn btn--dark btn--rounded btn--w-icon btn--github js-affix'><i class='icon icon--github' style='float: left; margin-left: 0px; margin-right: 16px'></i>Suggest Edits</a>";
 		var ghButton = $.parseHTML(ghButtonHtml);
 		$('.js-sections').append(ghButton);
 	}
