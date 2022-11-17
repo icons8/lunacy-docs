836f0f1fb62b20ac0fab8cc9715c8caf6135f2a3
Yaroslav Yudinskikh <35652747+YarikSWT@users.noreply.github.com>
2022-11-17 14:33:56 +0300

    New search (#68)
    * fix links
    
    * add search
    
    * add baseUrl to links in search
    
    * small refactor
    
    * fix content style issues
    
    * fix search result styles
    
    * fix links and urls for new domain
    
    * Update CNAME
    
    * fix baseurl
    
    * CNAME
    
    * remove markdown from search
    
    * add algolia
    
    * fix ul font-size
    
    * working search
    
    * enchance search ui
    
    * customize loader
    
    * fix job
    
    * try to use cusom gh-pages publish
    
    * fix index job
    
    * fix build index
    
    * Update CNAME
    
    * return index_name to confdig
    
    * Create jekyll-gh-pages.yml
    
    * remove uselss action
    
    * come back
    
    * use another algolia account
    
    * fix tablet styles
    
    * move scripts and styles to footer and head
    
    * rm usless page
    
    * upgrade loading indicator
    
    Co-authored-by: yaroslav_yudi <yaskih@mail.ru>

--- docs/doks-theme/assets/js/search-modal.js
+++ docs/doks-theme/assets/js/search-modal.js
@@ -3,63 +3,144 @@
 jQuery(function () {
   $.modal.defaults.showClose = false;
 
-  // Retreive baseUrl from jekyull global variable
-  const baseUrl = "{% if jekyll.environment == 'production' %}{{ site.doks.baseurl }}{% endif %}"
+  const search = instantsearch({
+    indexName: "{{ site.algolia.index_name }}",
+    searchClient: algoliasearch(
+      "{{ site.algolia.application_id }}",
+      "{{ site.algolia.search_only_api_key }}"
+    ),
+  });
 
-  var searchIndex; //lunr search index
+  const hitTemplate = function (hit) {
+    let url = `{{ site.baseurl }}${hit.url}#${hit.anchor}`;
 
-  function displaySearchResults(results, store) {
-    let htmlString = "";
-    if (results.length) {
-      for (const result of results) {
-        const item = store[result.ref]
-        htmlString += `<li class="search-result">
-            <a href="${baseUrl + item.url}">
-                <div class="search-result-content">
-                    <h3 class="search-result-header">${item.title}</h3>
-                    <p class="search-result-description">${item.content.substring(0,150)}...</p>
-                </div>
-            </a>
-        </li>`;
-      }
-    } else {
-      htmlString = '<li class="no-result">No results found</li>';
+    const title = hit._highlightResult.title.value;
+
+    let breadcrumbs = "";
+    if (hit._highlightResult.headings) {
+      breadcrumbs = hit._highlightResult.headings
+        .map((match) => {
+          return `<span class="post-breadcrumb">${match.value}</span>`;
+        })
+        .join(" > ");
     }
-    $("#search-results-list").html(htmlString);
-  }
 
-  function buildIndex() {
-    // Initalize lunr with the fields it will be searching on. I've given title
-    searchIndex = lunr(function () {
-      this.field("id");
-      this.field("title", { boost: 10 });
-      this.field("content");
-    });
+    const content = hit._highlightResult.content.value;
 
-    for (var key in window.store) {
-      // Add the data to lunr
-      searchIndex.add({
-        id: key,
-        title: window.store[key].title,
-        content: window.store[key].content,
+    return `
+      <li class="search-result">
+        <a href="${url}"
+          <div class="search-result-content">
+              <h3 class="search-result-header">
+                ${title}
+              </h3>
+              <p class="post-breadcrumbs">${breadcrumbs}</p>
+              <div class="search-result-description">${content}</div>
+          </div>
+        </a>
+      </li>
+    `;
+  };
+
+  const { connectHits, connectSearchBox } = instantsearch.connectors;
+  const { poweredBy } = instantsearch.widgets;
+
+  const renderHits = (renderOptions, isFirstRender) => {
+    const { hits, widgetParams, results } = renderOptions;
+
+    if (isFirstRender || !results.query) {
+      return;
+    }
+
+    widgetParams.container.innerHTML = `
+    <hr class="divider">
+      <ul class="search-results__list unstyled" id="search-results-list">
+        ${hits
+          .map((item) => {
+            return hitTemplate(item);
+          })
+          .join("")}
+      </ul>
+    `;
+  };
+
+  const renderSearchBox = (renderOptions, isFirstRender) => {
+    const { query, refine, clear, isSearchStalled, widgetParams } =
+      renderOptions;
+
+    if (isFirstRender) {
+      const input = document.createElement("input");
+
+      const loadingContainer = document.createElement("div");
+      loadingContainer.setAttribute('class', 'loader-container')
+      const loadingIndicator = document.createElement("div");
+      loadingIndicator.setAttribute('class', 'loader')
+      loadingContainer.appendChild(loadingIndicator)
+
+      const button = document.createElement("button");
+      button.innerHTML =
+        '<img src="/doks-theme/assets/images/layout/close.svg" />';
+      button.setAttribute("id", "search-input-clear");
+      button.setAttribute("style", "display: none;");
+
+      input.addEventListener("input", (event) => {
+        refine(event.target.value);
+        $("#search-input-clear").show();
       });
+
+      input.setAttribute("id", "search-modal-input");
+      input.setAttribute("class", "search-input");
+
+      button.addEventListener("click", () => {
+        clear();
+        $("#search-input-clear").hide();
+        $("#search-results-list").html("");
+      });
+
+      widgetParams.container.appendChild(input);
+      widgetParams.container.appendChild(button);
+      widgetParams.container.appendChild(loadingContainer);
+      input.focus();
     }
-  }
+
+    widgetParams.container.querySelector("input").value = query;
+    widgetParams.container.querySelector(".loader-container").hidden = !isSearchStalled;
+  };
+
+  // Create the custom widget
+  const customHits = connectHits(renderHits);
+  const customSearchBox = connectSearchBox(renderSearchBox);
+
+  search.addWidgets([
+    customSearchBox({
+      container: document.querySelector("#search-bar"),
+      autofocus: true,
+    }),
+    customHits({
+      container: document.querySelector("#search-results"),
+    }),
+    poweredBy({
+      container: document.querySelector("#powered-by"),
+    }),
+  ]);
 
   $("#site_search").click(function (event) {
     event.preventDefault();
     this.blur(); // Manually remove focus from clicked link.
+    const box = this.getBoundingClientRect() 
     $("#search-modal").modal();
-    $("#search-modal-input").focus();
-    if (!searchIndex) buildIndex();
+    let isMobile = window.matchMedia("only screen and (max-width: 820px)").matches;
+    $("#search-modal").css({'position': 'absolute', 'top': box.y, 'left': !isMobile ? box.x: '5vw'});
+    
+    try {
+      search.start();
+    } catch (error) {
+      search.refresh();
+      $("#search-modal-input").focus();
+    }
   });
 
-  $("#search-modal-input").on("input", function (event) {
-    const term = event.target.value.trim();
-    if (term) {
-      $("#search-results").show();
-      const results = searchIndex.search(term);
-      displaySearchResults(results, window.store);
-    }
+  $("#search-modal").on($.modal.BEFORE_CLOSE, () => {
+    $("#search-results-list").html("");
   });
 });
